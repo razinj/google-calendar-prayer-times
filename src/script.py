fb2f69ab00f7f7a663a45d242e1a41b592ae3d57
razinj <nizarjailane@gmail.com>
2024-11-24 17:03:53 +0100

    chore: fix linting + adjust linting rule

--- src/script.py
+++ src/script.py
@@ -88,7 +88,7 @@ def create_calendar_events(
             },
         }
 
-        # See: https://developers.google.com/resources/api-libraries/documentation/calendar/v3/python/latest/calendar_v3.events.html#insert
+        # See: https://developers.google.com/resources/api-libraries/documentation/calendar/v3/python/latest/calendar_v3.events.html#insert # noqa: E501
         event = service.events().insert(calendarId=CALENDAR_ID, body=event).execute()
         print(f"Event created: {event.get('summary')}")
 
